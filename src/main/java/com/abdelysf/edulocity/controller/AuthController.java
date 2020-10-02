5b4c70729f4919c833d6a7e9a43ebefa00972a87
abdelysf <abdel.ysf@gmail.com>
2020-10-02 19:55:15 +0100

    account verification

--- src/main/java/com/abdelysf/edulocity/controller/AuthController.java
+++ src/main/java/com/abdelysf/edulocity/controller/AuthController.java
@@ -18,16 +18,20 @@ public class AuthController {
     final AuthService authService; // DI by constructor , better than @autowired
 
     @PostMapping("/signup/student")
-    public ResponseEntity signUpStudent(@RequestBody RegisterStudentRequest studentRequest){
+    public ResponseEntity<String> signUpStudent(@RequestBody RegisterStudentRequest studentRequest){
         authService.signUpStudent(studentRequest);
         return new ResponseEntity<String>("l'inscription s'est deroulé avec succes vérifier votre email pour activer votre compte ", HttpStatus.OK);
     }
     @PostMapping("/signup/instructor")
-    public ResponseEntity signUpInstructor(@RequestBody RegisterInstructorRequest instructorRequest){
+    public ResponseEntity<String> signUpInstructor(@RequestBody RegisterInstructorRequest instructorRequest){
         authService.signUpInstructor(instructorRequest);
         return new ResponseEntity<String>("l'inscription s'est deroulé avec succes vérifier votre email pour activer votre compte ", HttpStatus.OK);
 
     }
 
-
+    @GetMapping("accountVerification/{token}")
+    public ResponseEntity<String>  verifyAccount(@PathVariable String token){
+        authService.verifyAccount(token);
+        return new ResponseEntity<String>("le compte était activé avec succès  ",HttpStatus.OK);
+    }
 }
